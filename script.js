0d75cb9e43835bc4948464eab66a57e3d789dca1
chem-e <nooru612@gmail.com>
2018-10-19 13:39:51 +0500

    ii

---
+++ script.js
@@ -0,0 +1,11 @@
+let options={
+	duration:2000
+}
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
+     
